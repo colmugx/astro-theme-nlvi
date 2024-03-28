816ee4706b8dd887beec59b6b46aeed2a63685e3
colmugx <colmugx@qq.com>
2024-03-28 12:55:08 +0800

    feat: post service

--- src/model/category.ts
+++ src/model/category.ts
@@ -1,4 +1,5 @@
 export interface Category {
   id: string
+  slug: string
   title: string
 }
---
+++ src/service/post.ts
@@ -0,0 +1,53 @@
+import type { Category } from '../model/category'
+import { getCollection, type CollectionEntry } from 'astro:content'
+
+class PostService {
+  private static Instance: PostService
+  static getInstance() {
+    if (!this.Instance) {
+      this.Instance = new PostService()
+    }
+
+    return this.Instance
+  }
+
+  private data: CollectionEntry<'blog'>[] = []
+
+  constructor() {
+    this.init()
+  }
+
+  list() {
+    return this.data.sort((a, b) => {
+      const prevDate = a.data.date || a.data.pubDate
+      const nextDate = b.data.date || b.data.pubDate
+      return nextDate!.valueOf() - prevDate!.valueOf()
+    })
+  }
+
+  getCategories() {
+    return Array.from(new Set(this.data.map(item => item.data.category || '')))
+      .filter(Boolean)
+      .map<Category>(item => ({
+        id: item,
+        title: item,
+        slug: `categories/${item}`,
+      }))
+  }
+
+  getMenu() {
+    const Index: Category = {
+      id: '0',
+      title: 'Posts',
+      slug: '',
+    }
+
+    return [Index].concat(this.getCategories())
+  }
+
+  private async init() {
+    this.data = await getCollection('blog')
+  }
+}
+
+export const postService = PostService.getInstance()
