ce7823356d6202d784fe011714d4c743591a7811
colmugx <colmugx@qq.com>
2024-03-28 11:49:28 +0800

    feat: header

--- src/constant/app.ts
+++ src/constant/app.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Colmugx\'s\nBlog';
+export const SITE_TITLE = 'Colmugx\'s Space';
+export const DISPLAY_SITE_TITLE = 'Colmugx\'s\n Space';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
