6c5115b06b97569f52ad9f5e725a8822e0b112c9
colmugx <colmugx@qq.com>
2024-03-30 22:30:16 +0800

    feat: site lang

--- src/constant/app.ts
+++ src/constant/app.ts
@@ -1,6 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Colmugx\'s Space';
-export const DISPLAY_SITE_TITLE = 'Colmugx\'s\n Space';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+import type { LanguageTag } from './_lang'
+
+export const SITE_TITLE = "Colmugx's Space"
+export const SITE_LANG: LanguageTag = 'zh-CN'
+
+export const DISPLAY_SITE_TITLE = "Colmugx's\n Space"
+export const SITE_DESCRIPTION = 'Welcome to my website!'
+
