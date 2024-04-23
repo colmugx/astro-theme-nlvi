e955649a8acbdcf554efe7ce452e95ca833fa139
colmugx <colmugx@qq.com>
2024-04-23 16:43:40 +0800

    feat: keyframes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,17 @@ export default {
       fontFamily: {
         texture: "'AmericanCaptain', ui-sans-serif, system-ui, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
         bodytext: "'Helvetica Neue', Helvetica,Arial, 'Microsoft Yahei', 'Hiragino Sans GB', 'Heiti SC', 'WenQuanYi Micro Hei', sans-serif"
+      },
+      keyframes: ({ theme }) => ({
+        flashBtn: {
+          '0%, 20%': { 'background-color': 'transparent', color: theme('colors.main') },
+          '10%': { 'background-color': theme('colors.main'), color: 'transparent' },
+          '30%': { 'background-color': theme('colors.main'), color: 'transparent', opacity: 0 },
+          '100%': { 'background-color': theme('colors.main'), color: '#fff', opacity: 1 },
+        }
+      }),
+      animation: {
+        flashBtn: 'flashBtn 280ms ease forwards'
       }
     },
   },
