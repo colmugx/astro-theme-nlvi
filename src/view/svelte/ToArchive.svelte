4f8c6565d8dbf5ebbaaa4b4f9e801293d6df3454
colmugx <colmugx@qq.com>
2024-04-23 16:36:52 +0800

    feat: archive buttion

--- src/view/svelte/ToArchive.svelte
+++ src/view/svelte/ToArchive.svelte
@@ -1,3 +1,9 @@
-<a href="/archive">
-  <div>ARCHIVE</div>
+<a href="/archive" aria-label="Go to archive page">
+  <div class="uppercase m-8 flex justify-center">
+    <div
+      class="pt-2 px-2 text-main font-texture text-4xl hover:animate-flashBtn rounded-sm"
+    >
+      archive {'...'}
+    </div>
+  </div>
 </a>
