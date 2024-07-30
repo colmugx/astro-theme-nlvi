e6bdc3b529f23e834797b81368290e459b347ecd
colmugx <colmugx@qq.com>
2024-07-30 15:48:23 +0800

    feat: refactor components and move to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
 import icon from "astro-icon";
 import { remarkReadingTime } from './remark-plugin/reading-time';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), svelte(), icon()],
+  integrations: [mdx(), sitemap(), tailwind(), icon()],
   markdown: {
     remarkPlugins: [remarkReadingTime]
   }
